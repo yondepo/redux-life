7c31eddf99732ba8ef58e418f05ea045f573d36a
Ruslan Karaev <yondepo@gmail.com>
2016-07-29 12:25:55 +0500

    setting up

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 /* eslint-disable import/default */
 import 'babel-polyfill';
 import React from 'react';
-import { render } from 'react-dom';
+import {render} from 'react-dom';
 import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
-import { Router, browserHistory } from 'react-router';
-import routes from './routes';
-import {loadCourses} from './actions/courseActions';
-import {loadAuthors} from './actions/authorActions';
 import './styles/style.scss';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-
-
